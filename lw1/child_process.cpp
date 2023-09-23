945bfcc01bbcbe39ed9bec509732299271cdebe5
Sky <terebaev1234@gmail.com>
2023-09-23 03:17:52 +0300

    project

---
+++ lw1/child_process.cpp
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int file_d = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+    if (file_d == -1) {
+        cerr << "Creating file error" << endl;
+        return 1;
+    }
+    
+    if (dup2(file_d, fileno(stdout)) == -1) {
+        cerr << "Dup2 error" << endl;
+        return 1;
+    }
+    string line;
+
+    // while (getline(cin, line)) {
+    //     float ans = 0.0, number = 0.0;
+    //     istringstream iss(line);
+    //     while (iss >> number) {
+    //         ans += number;
+    //     }
+    //     cout << ans << endl;
+    // }
+
+    char c = ' ';
+    float ans = 0.0, number = 0.0;
+    while (scanf("%f%c", &number, &c) > 0) {
+        ans += number;
+        if (c == '\n') {
+            cout << ans << endl;
+            ans = 0.0;
+        }
+    }
+
+    close(file_d);
+    return 0;
+}
---
+++ lw1/main.cpp
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    int pipe1[2], pipe2[2];
+
+    if (pipe(pipe1) == -1 or pipe(pipe2) == -1) {
+        cerr << "Pipe error" << endl;
+        return 1;
+    }
+    string filename;
+    getline(cin, filename);
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        cerr << "Fork error" << endl;
+        return 1;
+    } else if (pid == 0) {
+        close(pipe1[1]);
+        close(pipe2[0]);
+
+        if (dup2(pipe1[0], fileno(stdin)) == -1 or dup2(pipe2[1], fileno(stdout)) == -1) {
+            cerr << "Dup2 error" << endl;
+            return 1;
+        }
+
+        if (execl("./child_process", "./child_process", filename.c_str(), NULL) == -1) {
+            cerr << "Exec error" << endl;
+            return 1;
+        }
+        
+        close(pipe1[0]);
+        close(pipe2[1]);
+
+    } else {
+        close(pipe1[0]);
+        close(pipe2[1]);
+
+        string line;
+        while(getline(cin, line)) {
+            if (line == "exit") {
+                break;
+            }
+            line += "\n";
+            write(pipe1[1], line.c_str(), strlen(line.c_str()));
+        }
+
+        close(pipe1[1]);
+        close(pipe2[0]);
+        wait(nullptr);
+    }
+    
+    return 0;
+}
