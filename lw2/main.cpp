137f45584abc256598c0ded2ec7059380ca6c061
Sky <terebaev1234@gmail.com>
2023-11-12 04:16:31 +0300

    the first version

--- lw2/main.cpp
+++ lw2/main.cpp
@@ -1,136 +1,158 @@
 #include <fcntl.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-// #include <algorithm>
+#include <chrono>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-struct arg_t {
-    int start, finish, id, n, m;
-    vector<vector<double>>* matrix;
-    vector<double>* solve;
+struct Args {
+    int tid;
+    int num_threads;
+    float *matrix;
+    int columns;
+    pthread_barrier_t *barrier;
 };
 
-void input(int& n, int& m, vector<vector<double>>& matrix, vector<double>& solve, vector<arg_t>& args, int& thread_count) {
-    // cin >> n;
-    // m = n + 1;
+void *ge_parallel(void *args) {
+    Args *local_args = (Args *)args;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
+    int tid = local_args->tid;
+    int num_threads = local_args->num_threads;
+    float *matrix = local_args->matrix;
+    int columns = local_args->columns;
+    pthread_barrier_t *barrier = local_args->barrier;
 
-    for (int i = 0; i < thread_count; ++i) {
-        args[i].id = i;
-        args[i].n = n;
-        args[i].m = m;
-        args[i].matrix = &matrix;
-        args[i].solve = &solve;
-        args[i].start = n / thread_count * i;
-        args[i].finish = n / thread_count * (i + 1);
-    }
-}
+    for (int i = 0; i < columns - 1; i++) {
+        if ((i % num_threads) == tid) {
+            float pivot = matrix[i * columns + i];
 
-void* forward_elimination(void* arg) {
-    arg_t* args{static_cast<arg_t*>(arg)};
+            for (int j = i + 1; j < columns; j++) {
+                matrix[i * columns + j] /= pivot;
+            }
 
-    int start = min(args->start, args->n);
-    int finish = min(args->finish + 1, args->n);
-
-    for (int i = start; i < finish; i++) {
-        double tmp = (*args->matrix)[i][i];
-        for (int j = args->n; j >= i; j--) {
-            (*args->matrix)[i][j] /= tmp;
+            matrix[i * columns + i] = 1;
         }
 
-        for (int j = i + 1; j < finish; j++) {
-            tmp = (*args->matrix)[j][i];
-            for (int k = args->n; k >= i; k--) {
-                (*args->matrix)[j][k] -= tmp * (*args->matrix)[i][k];
+        pthread_barrier_wait(barrier);
+
+        for (int j = i + 1; j < columns; j++) {
+            if ((j % num_threads) == tid) {
+                float scale = matrix[j * columns + i];
+
+                for (int l = i + 1; l < columns; l++) {
+                    matrix[j * columns + l] -= matrix[i * columns + l] * scale;
+                }
+
+                matrix[j * columns + i] = 0;
             }
         }
     }
-    pthread_exit(0);
+
+    if (tid == num_threads - 1) {
+        matrix[(columns - 1) * columns + columns - 1] = 1;
+    }
+
+    return 0;
 }
 
-void* back_substitution(void* arg) {
-    arg_t* args{static_cast<arg_t*>(arg)};
+void launch_threads(int num_threads, float *matrix, float *solve, int rows, int columns) {
+    pthread_t *threads = new pthread_t[num_threads];
 
-    int start = min(args->start, args->n);
-    int finish = min(args->finish + 1, args->n);
+    pthread_barrier_t barrier;
+    pthread_barrier_init(&barrier, NULL, num_threads);
 
-    (*args->solve)[finish - 1] = (*args->matrix)[finish - 1][finish];
-    for (int i = finish - 2; i >= 0; i--) {
-        (*args->solve)[i] = (*args->matrix)[i][finish];
-        for (int j = i + 1; j < finish; j++) {
-            (*args->solve)[i] -= (*args->matrix)[i][j] * (*args->solve)[j];
+    Args *thread_args = new Args[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_args[i].tid = i;
+        thread_args[i].num_threads = num_threads;
+        thread_args[i].matrix = matrix;
+        thread_args[i].columns = columns;
+        thread_args[i].barrier = &barrier;
+
+        pthread_create(&threads[i], NULL, ge_parallel, (void *)&thread_args[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    solve[rows - 1] = matrix[rows * rows + rows - 1];
+    for (int i = rows - 2; i >= 0; i--) {
+        solve[i] = matrix[i * (rows + 1) + rows];
+        for (int j = i + 1; j < rows; j++) {
+            solve[i] -= matrix[i * (rows + 1) + j] * solve[j];
         }
     }
 
-    pthread_exit(0);
+    delete[] threads;
+    delete[] thread_args;
 }
 
-int main(int argc, char* argv[]) {
-    int file_d = open("input.txt", O_RDONLY);
-    // int file_d = open("input100.csv", O_RDONLY);
-    // int file_d = open("input2000.csv", O_RDONLY);
-    // int file_d = open("input2500.csv", O_RDONLY);
-    // int file_d = open("input3000.csv", O_RDONLY);
-
-    if (file_d == -1) {
-        cerr << "File error" << endl;
-        return 1;
-    }
-
-    if (dup2(file_d, fileno(stdin)) == -1) {
-        cerr << "Dup2 error" << endl;
-        return 1;
-    }
-
-    int thread_count = stoi(argv[1]);
-    if (thread_count < 1) {
+int main(int argc, char *argv[]) {
+    int num_threads = stoi(argv[1]);
+    if (argc < 1) {
         cerr << "At least 1 thread must exist" << endl;
         return 1;
     }
 
-    int n, m;
-    cin >> n;
-    m = n + 1;
+    int output_file_d = open("../InOutFiles/output.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+    // int input_file_d = open("../InOutFiles/10.txt", O_RDONLY);
+    // int input_file_d = open("../InOutFiles/2500.txt", O_RDONLY);
+    int input_file_d = open("../InOutFiles/3000.txt", O_RDONLY);
 
-    thread_count = min(thread_count, n);
-    vector<arg_t> args(thread_count);
-    vector<pthread_t> tid(thread_count);
-    vector<double> solve(n, 0);
-    vector<vector<double>> matrix(n, vector<double>(m, 0));
-
-    input(n, m, matrix, solve, args, thread_count);
-
-    for (int i = 0; i < thread_count; ++i) {
-        pthread_create(&tid[i], nullptr, forward_elimination, static_cast<void*>(&args[i]));
+    if (input_file_d == -1 || output_file_d == -1) {
+        cerr << "File error" << endl;
+        return 1;
     }
 
-    for (int i = 0; i < thread_count; ++i) {
-        pthread_join(tid[i], nullptr);
+    if (dup2(input_file_d, fileno(stdin)) == -1) {
+        cerr << "Dup2 error" << endl;
+        return 1;
     }
 
-    for (int i = 0; i < thread_count; ++i) {
-        pthread_create(&tid[i], nullptr, back_substitution, static_cast<void*>(&args[i]));
+    int rows, columns;
+    cin >> rows;
+    columns = rows + 1;
+
+    float *matrix;
+    float *solve;
+
+    matrix = new float[columns * columns];
+    solve = new float[rows];
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            cin >> matrix[i * columns + j];
+        }
     }
 
-    for (int i = 0; i < thread_count; ++i) {
-        pthread_join(tid[i], nullptr);
+    chrono::high_resolution_clock::time_point start;
+    chrono::high_resolution_clock::time_point end;
+
+    start = chrono::high_resolution_clock::now();
+    launch_threads(num_threads, matrix, solve, rows, columns);
+    end = chrono::high_resolution_clock::now();
+
+    chrono::duration<float> elapsed = chrono::duration_cast<chrono::duration<float>>(end - start);
+    cout << "Time = " << elapsed.count() << " seconds" << endl;
+
+    if (dup2(output_file_d, fileno(stdout)) == -1) {
+        cerr << "Dup2 error" << endl;
+        return 1;
     }
 
-    cout << "Solve" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "x" << i + 1 << " = " << solve[i] << endl;
+    for (int i = 0; i < rows; i++) {
+        cout << solve[i] << endl;
     }
 
-    close(file_d);
+    delete[] matrix;
+    delete[] solve;
+    close(input_file_d);
+    close(output_file_d);
 
     return 0;
 }
